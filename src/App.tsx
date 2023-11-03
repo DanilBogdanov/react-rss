a6936cd9dab70389e62b1986767bd6530c6f36da
DanilWork <bogdanovdanila1404@gmail.com>
2023-11-03 13:21:10 +0500

    refactor: change App to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
+import React, { useState } from 'react';
 import CharacterList from './components/characterList/CharacterList';
 import SearchBar from './components/searchBar/SearchBar';
-import React from 'react';
-import './App.css';
 import ErrorBoundary from './components/errorBoundary/errorBoundary';
+import './App.css';
 
-type AppState = {
-  query: string | null;
-};
+export default function App(): JSX.Element {
+  const [query, setQuery] = useState<string | null>(null);
 
-export default class App extends React.Component<object, AppState> {
-  state: AppState = {
-    query: null,
-  };
-
-  onChangeQuery(q: string) {
-    this.setState({ query: q });
+  function onChangeQuery(q: string) {
+    setQuery(q);
   }
 
-  render() {
-    return (
-      <ErrorBoundary>
-        <header>
-          <h1>Rick and Morty</h1>
-          <SearchBar
-            onChange={(query) => this.onChangeQuery(query)}
-          ></SearchBar>
-        </header>
-        <main className='main'>
-          <CharacterList name={this.state.query} />
-        </main>
-      </ErrorBoundary>
-    );
-  }
+  return (
+    <ErrorBoundary>
+      <header>
+        <h1>Rick and Morty</h1>
+        <SearchBar onChange={(query) => onChangeQuery(query)}></SearchBar>
+      </header>
+      <main className='main'>
+        <CharacterList name={query} />
+      </main>
+    </ErrorBoundary>
+  );
 }
