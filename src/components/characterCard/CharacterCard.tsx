707e67ac26c9ccbde1438afde9f84155add4c7c0
DanilWork <bogdanovdanila1404@gmail.com>
2023-11-03 15:22:27 +0500

    refactor: change CharacterCard to functional

--- src/components/characterCard/CharacterCard.tsx
+++ src/components/characterCard/CharacterCard.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Character } from '../../types/api';
 import './characterCard.css';
 
@@ -6,15 +5,13 @@ type CharacterListProps = {
   character: Character;
 };
 
-export default class CharacterCard extends React.Component<CharacterListProps> {
-  render() {
-    return (
-      <div className='character-card'>
-        <h3>{this.props.character.name}</h3>
-        <p>Species: {this.props.character.species}</p>
-        <p>Sex: {this.props.character.gender}</p>
-        <img src={this.props.character.image} />
-      </div>
-    );
-  }
+export default function CharacterCard({
+  character,
+}: CharacterListProps): JSX.Element {
+  return (
+    <div className='character-card'>
+      <h4 className='character-card__title'>{character.name}</h4>
+      <img src={character.image} />
+    </div>
+  );
 }
