881d9b23af8efa98986d6aa2389ec21bbce3bf0d
Danil HP <bogdanovdanila1404@gmail.com>
2023-11-13 00:48:31 +0500

    feat: add notFoundPage test

---
+++ src/components/pages/notFoundPage/NotFoundPage.test.tsx
@@ -0,0 +1,27 @@
+import { act, render, screen } from '@testing-library/react';
+import NotFoundPage from './NotFoundPage';
+import AppRouter from '@/router/AppRouter';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('NotFoundPage test', () => {
+  test('Title is displayed', () => {
+    render(<NotFoundPage />);
+    expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+      'Page not found'
+    );
+  });
+
+  test('404 page is displayed when navigating to an invalid route', async () => {
+    await act(async () => {
+      render(
+        <MemoryRouter initialEntries={['/incorrectPage']}>
+          <AppRouter />
+        </MemoryRouter>
+      );
+    });
+
+    expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+      'Page not found'
+    );
+  });
+});
