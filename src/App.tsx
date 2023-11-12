c1239f2f480d661622640f66e39b5cc446997a25
Danil HP <bogdanovdanila1404@gmail.com>
2023-11-13 00:43:50 +0500

    feat: separate routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import ErrorBoundary from '@/components/errorBoundary/errorBoundary';
-import CharacterPage from '@/components/pages/characterPage/CharacterPage';
-import SearchPage from '@/components/pages/searchPage/searchPage';
 import './App.css';
+import AppRouter from './router/AppRouter';
 
 export default function App(): JSX.Element {
   return (
     <ErrorBoundary>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Navigate to='/search' />} />
-          <Route path='/search' element={<SearchPage />}>
-            <Route path='character/:id' element={<CharacterPage />} />
-          </Route>
-        </Routes>
+        <AppRouter />
       </BrowserRouter>
     </ErrorBoundary>
   );
