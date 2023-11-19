c15b2c31d8defd5ceec262cb5f5d5704de1f82a0
Danil HP <bogdanovdanila1404@gmail.com>
2023-11-19 17:47:20 +0500

    feat: change endpoint

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import CharacterPage from '@/components/pages/characterPage/CharacterPage';
+import ProductPage from '@/components/pages/productPage/ProductPage';
 import NotFoundPage from '@/components/pages/notFoundPage/NotFoundPage';
 import SearchPage from '@/components/pages/searchPage/searchPage';
 
@@ -8,7 +8,7 @@ export default function AppRouter(): JSX.Element {
     <Routes>
       <Route path='/' element={<Navigate to='/search' />} />
       <Route path='/search' element={<SearchPage />}>
-        <Route path='character/:id' element={<CharacterPage />} />
+        <Route path='product/:id' element={<ProductPage />} />
       </Route>
       <Route path='*' element={<NotFoundPage />} />
     </Routes>
