73f4542d8f6bce5dad8a4786243425abe425d3cc
Danil HP <bogdanovdanila1404@gmail.com>
2023-11-19 17:38:24 +0500

    refactor: change character to product

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,28 +1,27 @@
-export interface Character {
-  id: number;
-  name: string;
-  status: string;
-  species: string;
-  gender: string;
-  image: string;
-  url: string;
-}
-
-export interface CharacterResponse {
-  info: {
-    count: number;
-  };
-  results: Character[];
-}
-
-export interface CharacterRequest {
-  name: string;
-  page: number;
+export interface ProductRequest {
+  q: string;
+  skip: number;
   limit: PageLimit;
 }
 
 export enum PageLimit {
-  L20 = 1,
-  L40 = 2,
-  L60 = 3,
+  L20 = 20,
+  L40 = 40,
+  L60 = 60,
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  brand: string;
+  images: string[];
+}
+
+export interface ProductsResponse {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
 }
