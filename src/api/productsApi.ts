4cb5f16b6fac71bacd64469f339103cba3ae3c5d
Danil HP <bogdanovdanila1404@gmail.com>
2023-11-19 17:40:18 +0500

    feat: change api to rtk query

---
+++ src/api/productsApi.ts
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Product, ProductRequest, ProductsResponse } from '@/types/api';
+
+export const productsApi = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com' }),
+  endpoints: (build) => ({
+    fetchProduct: build.query<Product, string>({
+      query: (id: string) => ({
+        url: `products/${id}`,
+      }),
+    }),
+    fetchProducts: build.query<ProductsResponse, ProductRequest>({
+      query: ({ limit, skip, q }: ProductRequest) => ({
+        url: 'products/search',
+        params: { q, limit, skip },
+      }),
+    }),
+  }),
+});
+
+export const { useFetchProductQuery, useFetchProductsQuery } = productsApi;
