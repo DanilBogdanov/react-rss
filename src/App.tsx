1ac2e4547802006bc55f3d134e242c7a7af0d815
Danil HP <bogdanovdanila1404@gmail.com>
2023-11-19 17:47:55 +0500

    feat: add store provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { setupStore } from './store/store';
 import AppRouter from './router/AppRouter';
 import ErrorBoundary from '@/components/errorBoundary/errorBoundary';
 import './App.css';
 
+const store = setupStore();
+
 export default function App(): JSX.Element {
   return (
     <ErrorBoundary>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </Provider>
     </ErrorBoundary>
   );
 }
