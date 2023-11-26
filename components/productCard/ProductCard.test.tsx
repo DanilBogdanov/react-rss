8c144b7690c321592ffd45a008ac1c7908feae2c
Danil HP <bogdanovdanila1404@gmail.com>
2023-11-27 02:22:04 +0500

    feat: add product card test

---
+++ components/productCard/ProductCard.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from '@testing-library/react';
+import ProductCard from './ProductCard';
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+    };
+  },
+}));
+
+const testProduct = {
+  id: 1,
+  title: 'iPhone 9',
+  description: 'An apple mobile which is nothing like apple',
+  price: 549,
+  discountPercentage: 12.96,
+  rating: 4.69,
+  stock: 94,
+  brand: 'Apple',
+  category: 'smartphones',
+  thumbnail: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
+  images: [
+    'https://i.dummyjson.com/data/products/1/1.jpg',
+    'https://i.dummyjson.com/data/products/1/2.jpg',
+    'https://i.dummyjson.com/data/products/1/3.jpg',
+    'https://i.dummyjson.com/data/products/1/4.jpg',
+    'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
+  ],
+};
+
+describe('ProductCard renders the relevant card data', () => {
+  beforeEach(() => {
+    render(<ProductCard product={testProduct} />);
+  });
+
+  test('ProductCard is presence', () => {
+    expect(screen.getByTestId('product-card')).toBeInTheDocument();
+  });
+
+  test('ProductCard show product name', () => {
+    expect(screen.getByRole('heading', { level: 4 }).textContent).toEqual(
+      testProduct.title
+    );
+  });
+
+  test('ProductCard show product img', () => {
+    const img = screen.getByRole('img');
+
+    expect(img).toHaveAttribute('src', testProduct.images[0]);
+    expect(img).toHaveAttribute('alt', `${testProduct.title}-img`);
+  });
+});
