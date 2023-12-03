6dd91e699ea26c1380139c97b27a2f465fcdc593
Danil HP <bogdanovdanila1404@gmail.com>
2023-12-04 01:21:30 +0500

    feat: add uncontrolled form

---
+++ src/pages/UncontrolledFormPage.tsx
@@ -0,0 +1,10 @@
+import UncontrolledForm from '@/components/forms/uncontrollForm/UncontrolledForm';
+
+export default function UncontrolledFormPage(): JSX.Element {
+  return (
+    <div>
+      <h2>Uncontrolled Form Page</h2>
+      <UncontrolledForm />
+    </div>
+  );
+}
